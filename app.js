5feae1ec2ba41841f26654d8709919e615b7c98d
haya2001 <hayaakhtee33@gmail.com>
2021-04-18 02:16:20 +0500

    work accomplish

---
+++ app.js
@@ -0,0 +1,313 @@
+//Chapter 1
+
+//1. Write a script to greet your website visitor using JS alert box. 
+// alert("hey from haya");
+
+// 2. Write a script to display following message on your web page
+// alert("Error! Please enter a valid password");
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break) 
+// alert("Welcome to JS land... \n Happy Coding!");
+
+// 4. Write a script to display following messages in sequence: 
+// alert("welcome to JS land"); alert("Happy coding! \n Prevent this page for addional dialogue");
+
+// 5. Generate the following message through browser’s developer console: 
+// alert("Hello... i can run JS through my web browser's console");
+
+//chapter 2
+
+// 1. Declare a variable called username. 
+// var username ="Haya"
+
+// Declare a variable called myName & assign to it a string that represents your Full Name. 
+// var myName = "Haya Akhter"
+
+// 3. Write script to 
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
+    //var titledmessage ="Hello World"
+    //alert(titledmessage);
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes. 
+// var username ="Haya Akhter"
+// alert(username);
+// var age ="i am 19 years old"
+// alert(age);
+// var info ="certified Mobile Development Application"
+// alert(info);
+
+// 5. Write a script to display the following alert using one JS variable
+// var a = "Pizza \n Pizz \n Piz \n Pi \n P"
+// alert(a);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+// var email ="my email address is hayaakhtee33@gmail.com"
+// alert(email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+//  Display the following message in an alert box: 
+// var book = "I am trying to learn for book A smarter way to learn JavaScript"
+// alert(book);
+
+// 8. Write a script to display this in browser through JS 
+// var a = "YAH! I can write HTML content throught JavaScript"
+// document.write (a);
+
+// 9. Store following string in a variable and show in alert and browser through JS 
+// var a ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+// alert(a);
+
+// capter 3
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box
+// var age ="i am 19 years old"
+// alert(age);
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”
+
+
+
+
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser
+// var birthYear ="My birth year is 2002"
+// document.write(birthYear);
+//             document.write("<br>");
+//             document.write("Data type of my decleared varriable is number");
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var name = "John \n"
+// var Product ="T-shirt \n"
+// var Quantity ="\n 2 \n"
+// var result = name +"" + "ordered" + "" + Quantity + "" + Product + "" + "on XYZ clothing store."
+// document.write(result);
+
+// Chapter 4
+
+// 1. Declare 3 variables in one statement.
+// var a,b,c=""
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// legal variables
+// var a =""
+// var $ =""
+// var myName =""
+// var ____ =""
+// var e =""
+
+// illegal variables
+// var 1 =""
+// var .a =""
+// var 8* =""
+// var -o =""
+// var 13 =""
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ____letters__, ___numbers___,
+// __dollar signs____ and ___underscores___.
+// For example $my_1stVariable
+// c) Variables must begin with a __$text____, _text ____ or
+// __text___. For example $name, _name or name
+// d) Variable names are case ____sensitive_____
+// e) Variable names should not be JS ____keywords____
+
+// Chapter 5
+
+// 1. Write a program that take two numbers & add them in a 
+// new variable. Show the result in your browser.
+ 
+// var a = 5;
+// var b = 3;
+// var c = 5+3;
+// document.write("The Sum Of 5 and 3 is" +c)
+
+// 2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+    //    substraction
+    // var a = 5;
+    // var b = 3;
+    // var c = 5-3;
+    // document.write("The Subtraction Of 5 and 3 is" +c)
+
+     // multiplication
+    // var a = 5;
+    // var b = 3;
+    // var c = 5 * 3;
+    // document.write("The multiplication Of 5 and 3 is" +c)
+
+    // division
+    // var a = 5;
+    // var b = 3;
+    // var c = 5/3;
+    // document.write("The division Of 5 and 3 is" +c)
+
+    // modulus
+    // var a = 5;
+    // var b = 3;
+    // var c = 5 % 3;
+    // document.write("The module Of 5 and 3 is" +c)
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value
+// after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value 
+// by 3. 
+// l. Output : “The remainder is : 0”.    
+
+
+
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to 
+// store
+// ticket price in a variable & calculate the cost of buying 5 
+// tickets
+// to a movie. Example output
+
+
+
+
+// 5. Write a script to display multiplication table of any 
+// number in your browser
+// var a = 4*1;
+// var b = 4*2;
+// var c = 4*3;
+// var d = 4*4;
+// var e = 4*5;
+// var f = 4*6;
+// var g = 4*7;
+// var h = 4*8;
+// var i = 4*9;
+// var j = 4*10;
+// document.write("4x1=" +a)
+// document.write("<br>");
+// document.write("4x2=" +b)
+// document.write("<br>");
+// document.write("4x3=" +c)
+// document.write("<br>");
+// document.write("4x4=" +d)
+// document.write("<br>");
+// document.write("4x5=" +e)
+// document.write("<br>");
+// document.write("4x6=" +f)
+// document.write("<br>");
+// document.write("4x7=" +g)
+// document.write("<br>");
+// document.write("4x8=" +h)
+// document.write("<br>");
+// document.write("4x9=" +i)
+// document.write("<br>");
+// document.write("4x10=" +j)
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a 
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// var Celsius = prompt("enter temp in celsius")
+// var faranheit = 9/5*Celsius -32;
+// alert(faranheit);
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+// var faranheit = 12;
+// var celsius = 5/9* (faranheit -32);
+// alert(celsius);
+
+// 7. Write a program to implement checkout process of a 
+// shopping cart system for an e-commerce website. Store 
+// the following in variables
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+// Compute the total cost & show the receipt in your browser.
+
+
+
+// 8. Store total marks & marks obtained by a student in 2 
+// variables. Compute the percentage & show the result in 
+// your browser
+
+
+
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a 
+// script to convert the total currency to Pakistani Rupees. 
+// Perform all calculations in a single expression.
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee 
+// and 1 Saudi Riyal = 28 Pakistani Rupee
+
+
+
+// 10. Write a program to initialize a variable with some 
+// number and do arithmetic in following sequence:
+// a. Add 5
+// b. Multiply by 10
+// c. Divide the result by 2
+// Perform all calculations in a single expression
+
+
+
+// 11. The Age Calculator: Forgot how old someone is? 
+// Calculate it!
+// a. Store the current year in a variable.
+// b. Store their birth year in a variable.
+// c. Calculate their 2 possible ages based on the stored 
+// values.
+// Output them to the screen like so: “They are either NN or NN
+// years old”.
+
+
+// 12. The Geometrizer: Calculate properties of a circle.
+// a. Store a radius into a variable
+// b. Calculate the circumference based on the radius, and
+// output “The circumference is NN”.
+// (Hint : Circumference of a circle = 2 π r , π = 3.142)
+// Calculate the area based on the radius, and output “The 
+// area is NN”. (Hint : Area of a circle = π r2, π = 3.142
+
+
+
+// 13. The Lifetime Supply Calculator: Ever wonder how
+// much a “lifetime supply” of your favorite snack is? 
+// Wonder no more.
+// a. Store your favorite snack into a variable
+// b. Store your current age into a variable.
+// c. Store a maximum age into a variable.
+// d. Store an estimated amount per day (as a number).
+// e. Calculate how many would you eat total for the rest of
+// your life.
+// Output the result to the screen like so: “You will need 
+// NNNN to last you until the ripe old age of NN”
+
+
+
+
